7816de0940b6c3d46ec7bc2f050bb394db589426
William Firmino <williamwf07@gmail.com>
2020-01-19 15:24:26 -0300

    Para adicionar ao git

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://williamf:<password>@cluster0-dimdf.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
